d0bc6062849ce7c532ad6c11032335f5a204c823
programmersgateway <vaidhyanathansm99@gmail.com>
2020-01-06 21:36:10 +0530

    modified snake.py

--- snake.py
+++ snake.py
@@ -1,135 +1,183 @@
- 
 import pygame
-from pygame.locals import *
 import random
-# Colors
-BLACK = (0, 0, 0)
-BLUE = (0,0,255)
-WHITE = (255, 255, 255)
+import math
+import tkinter as tk
+from tkinter import messagebox 
+
+class cube(object):
+    rows = 20
+    w = 500
+    def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
+        self.pos = start
+        self.dirnx = 1
+        self.dirny = 0
+        self.color = color
+    def move(self,dirnx,dirny):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+    def draw(self,surface,eyes=False):
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
  
-# Set the width and height of each snake segment
-segment_width = 15
-segment_height = 15
-# Margin between each segment
-segment_margin = 3
+        pygame.draw.rect(surface, self.color, (i*dis+1,j*dis+1, dis-2, dis-2))
+        if eyes:
+            centre = dis//2
+            radius = 3
+            circleMiddle = (i*dis+centre-radius,j*dis+8)
+            circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
+
+class snake(object):
+    body = []
+    turns = {}
+    def __init__(self,color,pos):
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 0
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
  
-# Set initial speed
-x_change = segment_width + segment_margin
-y_change = 0
+                elif keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
  
+                elif keys[pygame.K_DOWN]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        for i, c in enumerate(self.body):           
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body)-1:
+                    self.turns.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
+                else: c.move(c.dirnx,c.dirny)
+
+    def reset(self,pos):
+        self.head = cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
+    def addCube(self):
+        tail = self.body[-1]
+        dx, dy = tail.dirnx, tail.dirny
  
-class Food():
-    #Class to print food
-    x_food = random.randint(0, 785)
-    y_food = random.randint(0, 585)
-    score = 0
+        if dx == 1 and dy == 0:
+            self.body.append(cube((tail.pos[0]-1,tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(cube((tail.pos[0]+1,tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cube((tail.pos[0],tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(cube((tail.pos[0],tail.pos[1]+1)))
+ 
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
+    def draw(self,surface):
+        for i, c in enumerate(self.body):
+            if i ==0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
+ 
+
+def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+        pygame.draw.line(surface, (255,255,255), (x,0), (x,w))
+        pygame.draw.line(surface, (255,255,255), (0,y), (w,y))
+        
 
 
-class Segment(pygame.sprite.Sprite):
-    # -- Methods
-    # Constructor function
-    def __init__(self, x, y):
-        # Call the parent's constructor
-        super().__init__()
- 
-        # Set height, width
-        self.image = pygame.Surface([segment_width, segment_height])
-        self.image.fill(BLUE)
- 
-        # Make our top-left corner the passed-in location.
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
- 
-# Call this function so the Pygame library can initialize itself
-pygame.init()
- 
-# Create an 800x600 sized screen
-screen = pygame.display.set_mode([800, 600])
- 
-myfont = pygame.font.Font(None, 40)
+def redrawWindow(surface):
+    global width, rows, s, snack 
+    surface.fill((0, 0, 0))
+    s.draw(surface)
+    snack.draw(surface)
+    drawGrid(width, rows, surface)
+    pygame.display.update()
 
+def randomSnack(rows, item):
+    positions = item.body
+ 
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
+            continue
+        else:
+            break
+       
+    return (x,y)
 
-# Set the title of the window
-pygame.display.set_caption('Snake Game')
- 
-allspriteslist = pygame.sprite.Group()
- 
-# Create an initial snake
-snake_segments = []
-for i in range(15):
-    x = 250 - (segment_width + segment_margin) * i
-    y = 30
-    segment = Segment(x, y)
-    snake_segments.append(segment)
-    allspriteslist.add(segment)
- 
- 
-clock = pygame.time.Clock()
-done = False
+def message_box(subject, content):
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+    try:
+        root.destroy()
+    except:
+        pass
 
-x_food = random.randint(0, 785)
-y_food = random.randint(0, 585)
-score = 0
-while not done:
- 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
- 
-        # Set the speed based on the key pressed
-        # We want the speed to be enough that we move a full
-        # segment, plus the margin.
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = (segment_width + segment_margin) * -1
-                y_change = 0
-            if event.key == pygame.K_RIGHT:
-                x_change = (segment_width + segment_margin)
-                y_change = 0
-            if event.key == pygame.K_UP:
-                x_change = 0
-                y_change = (segment_height + segment_margin) * -1
-            if event.key == pygame.K_DOWN:
-                x_change = 0
-                y_change = (segment_height + segment_margin)
-            if event.key == pygame.K_e:
-                print(score)
-            if y < y_food+30:
-                if x > x_food and x < x_food+30 or x+20 > x_food and x+20<x_food+30: 
-                    scoretext = myfont.render("Score {0}".format(score), 1, BLUE)
-                    screen.blit(scoretext, (50, 100))
-                    score += 1
+def main():
+    global width, rows, s, snack
+    width = 500
+    rows = 20
+    win = pygame.display.set_mode((width, width))
+    s =  snake((255, 0, 0),(10,10))
+    snack = cube(randomSnack(rows, s), color=(0,255,0))
+    flag = True
 
+    clock = pygame.time.Clock()
 
+    while flag:
+        pygame.time.delay(80)
+        clock.tick(8)
+        s.move()
+        if s.body[0].pos == snack.pos:
+            s.addCube()
+            snack = cube(randomSnack(rows, s), color=(0,255,0))
  
-    # Get rid of last segment of the snake
-    # .pop() command removes last item in list
-    old_segment = snake_segments.pop()
-    allspriteslist.remove(old_segment)
- 
-    # Figure out where new segment will be
-    x = snake_segments[0].rect.x + x_change
-    y = snake_segments[0].rect.y + y_change
-    segment = Segment(x, y)
- 
-    # Insert new segment into the list
-    snake_segments.insert(0, segment)
-    allspriteslist.add(segment)
- 
-    # -- Draw everything
-    # Clear screen
-    screen.fill(BLACK)
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
+                print('Score: ', len(s.body))
+                message_box('You Lost!', 'Play again...')
+                s.reset((10,10))
+                break
+        redrawWindow(win)
 
-    pygame.draw.rect(screen, BLUE, [x_food, y_food, 20, 20])
-
- 
-    allspriteslist.draw(screen)
- 
-    # Flip screen
-    pygame.display.flip()
- 
-    # Pause
-    clock.tick(5)
- 
-pygame.quit()
+main()
